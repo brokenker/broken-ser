2b0359cc245935480bc9a3d5bd0f020f5739f009
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-23 22:01:26 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,7 +9,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://i.imgur.com/aAGjt7z.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*SPlRlT SER bot created by SPlRlT *
 *Creator number : wa.me/18052196558?text=Hi%20splrlt%20ser.%20*
 
 *subscribe  my channel  : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA*
