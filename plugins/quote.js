f66c867353c683790b8dfb3c86daf16856e75c28
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-28 20:30:32 +0530

    Update quote.js

--- plugins/quote.js
+++ plugins/quote.js
@@ -1,79 +1,23 @@
-/* Copyright (C) 2021 Vai838.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsAsenaDuplicated
-*/
-
-const Asena = require('../events');
+const Asena = require('../events');   
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
-const Config = require('../config');
 
-const Language = require('../language');
-const Lang = Language.getString('weather');
+// List
+const QUOTE_DESC = "It Sends Random Quote"
+const NEED_LOCATIONA = "*Invalid Request*"
+const QUOTE = "Quote :"
+const AUTHOR = "Author :"
+const NOT_FOUNDA = "```Sorry,I could not find a quote. 😖```"
 
-
-
-
-
-
-if (Config.WORKTYPE == 'private') {
-
-Asena.addCommand({pattern: 'quote ?(.*)', fromMe: true, desc: Lang.QUOTE_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+Asena.addCommand({pattern: 'quote ?(.*)', fromMe: false, desc: QUOTE_DESC}, async (message, match) => {
+	if (match[1] === 'xx') return await message.reply(NEED_LOCATIONA);
 	const url = `https://api.quotable.io/random`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.QUOTE +'* ```' + json.content + '```\n\n' +
-		'*✒️' + Lang.AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + QUOTE +'* ```' + json.content + '```\n\n' +
+		'*✒️' + AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
 	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDA, MessageType.text);
+		return await message.client.sendMessage(message.jid, NOT_FOUNDA, MessageType.text);
 	}
 });
-
-
-Asena.addCommand({pattern: 'pquote ?(.*)', fromMe: true, dontAddCommandList: true}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.quotable.io/random`;
-	try {
-		const response = await got(url);
-		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.QUOTE +'* ```' + json.content + '```\n\n' +
-		'*✒️' + Lang.AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
-	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDA, MessageType.text);
-	}
-});
-}
-
-else if (Config.WORKTYPE == 'public') {
-
-Asena.addCommand({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.quotable.io/random`;
-	try {
-		const response = await got(url);
-		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.QUOTE +'* ```' + json.content + '```\n\n' +
-		'*✒️' + Lang.AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
-	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDA, MessageType.text);
-	}
-});
-
-
-Asena.addCommand({pattern: 'pquote ?(.*)', fromMe: true, dontAddCommandList: true}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.quotable.io/random`;
-	try {
-		const response = await got(url);
-		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.QUOTE +'* ```' + json.content + '```\n\n' +
-		'*✒️' + Lang.AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
-	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDA, MessageType.text);
-	}
-});
-}
-
