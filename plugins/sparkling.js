065d150393d2aa059ee3626fedac935dd1ff6535
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-28 17:51:56 +0530

    Update sparkling.js

--- plugins/sparkling.js
+++ plugins/sparkling.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 ameer-kallumthodi.
+/* Copyright (C) 2021 SPlRlT-YT.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-PIKACHU*/
+SPlRlT SER*/
 
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
@@ -16,9 +16,9 @@ if (Config.WORKTYPE == 'private') {
 
     if (match[1] === '') return await message.sendMessage(need);
 
-    var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/sparkling?text1=${encodeURIComponent(match[1])}&text2=Pikachu`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/sparkling?text1=${encodeURIComponent(match[1])}&text2=SPlRlT SER`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Pikachu*' })
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by SPlRlT SER*' })
 
   }));
 }
@@ -29,9 +29,9 @@ else if (Config.WORKTYPE == 'public') {
 
     if (match[1] === '') return await message.sendMessage(need);
 
-    var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/sparkling?text1=${encodeURIComponent(match[1])}&text2=Pikachu`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/sparkling?text1=${encodeURIComponent(match[1])}&text2=SPlRlT SER`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Pikachu*' })
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by SPlRlT SER*' })
 
   }));
 
