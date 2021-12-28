ca066fb1ce3447884c291a4273775887aab27c69
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-28 20:32:13 +0530

    Update qrgenerator.js

--- plugins/qrgenerator.js
+++ plugins/qrgenerator.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 farhan-dqz.
+/* Copyright (C) 2021 SPlRlT-YT.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsenaDuplicated
