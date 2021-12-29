d24de4b00ff101b63392fd1b2f4ddbd452a04715
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-29 21:53:33 +0530

    Spirit ser

--- plugins/git.js
+++ plugins/git.js
@@ -9,16 +9,16 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://i.imgur.com/qdkhgpq.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*SPlRlT SER bot created by SPlRlT *
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*SPIRIT SER bot created by SPIRIT *
 *Creator number : wa.me/18052196558?text=Hi%20splrlt%20ser.%20*
 
 *subscribe  my channel  : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA*
 
-*Githublink (Setup)  :    https://github.com/SPlRlT-YT/SPlRlT-SER*
+*Githublink (Setup)  :    https://github.com/SPlRlT-YT/Spirit-ser*
 
-*Audio commads :   https://github.com/SPlRlT-YT/SPlRlT-SER/tree/master/uploads*
+*Audio commads :   https://github.com/SPlRlT-YT/Spirit-ser/tree/master/uploads*
 
-*Sticker commads : https://github.com/SPlRlT-YT/SPlRlT-SER/tree/master/sticker*
+*Sticker commads : https://github.com/SPlRlT-YT/Spirit-ser/tree/master/sticker*
 
 *My channel : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA* 
 
